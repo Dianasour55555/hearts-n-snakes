117a8238cca0a8277471adf70ced6aaedab3e867
AlesyaMaluchenko <113368762+AlesyaMaluchenko@users.noreply.github.com>
2023-04-10 18:44:26 +0300

    Add files via upload
    code, that convert edf into txt and makes plots for 12 ECG leads

---
+++ converter.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import os
+from os import listdir
+import numpy as np
+import mne
+import matplotlib.pyplot as plt
+
+
+mypath = "C:/Users/Lenovo/PycharmProjects/pythonProject4/EDF/"
+newpath = "C:/Users/Lenovo/PycharmProjects/pythonProject4/"
+for f_name in listdir(mypath):
+    if f_name[9:] == 'edf':
+        f = mne.io.read_raw_edf(os.path.join(mypath, f_name))
+        header = ','.join(f.ch_names)
+        f_new_name = f_name[0: -3] + 'txt'
+        np.savetxt(os.path.join(newpath, f_new_name), f.get_data().T, delimiter=',', header=header)
+        pd_file = pd.read_csv(os.path.join(newpath, f_new_name), sep=',')
+        pd_file['time'] = np.arange(0.0, 10.0, 0.002)
+        for name in list(pd_file.columns)[0:12]:
+             plot = pd_file.plot(x='time', y=name)
+             plot_name = f_name[0: -3] + '_plot_' + '{}'.format(name) + '.png'
+             plt.savefig(plot_name)
+
+
+
