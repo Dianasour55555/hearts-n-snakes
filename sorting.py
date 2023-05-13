6053c1888ac0c8c70a0446f3ebf864c66dcc3de6
Dianasour55555 <113368592+Dianasour55555@users.noreply.github.com>
2023-05-13 20:03:13 +0300

    Update sorting.py

--- sorting.py
+++ sorting.py
@@ -7,15 +7,17 @@ import shutil
 from os import path
 
 
-for f_name in listdir():
+for f_name in listdir('Testovaya_baza2'):
     if f_name[9:] == 'txt':
-        pd_file = pd.read_csv(os.path.join(path, f_name), sep=',')
+        pd_file = pd.read_csv(os.path.join(f_name), sep=',')
         F = np.fft.rfft(pd_file['ECG II-Ref'], n=None, axis=-1)
         A = [((F[i].real) ** 2 + (F[i].imag) ** 2) ** 0.5 for i in np.arange(100, 1500, 1)]  # модуль амплитуды
         freq = (A.index(max(A)) + 100) / 500
         if freq < 0.99:
-            name =
-            new_location = shutil.move(source_path, 'brad')
+            name = f_name[:9] + '._plot_ECG II-Ref.png'
+            path.exists(name)
+            new_location = shutil.move(name, 'brad')
+
         elif freq > 1.7:
             pass
         else:
